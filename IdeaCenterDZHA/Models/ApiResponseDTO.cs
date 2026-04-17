a3d1a704bf056c4cee7eadcc059d2f0ff3b11500
dimitar0811 <160605749+dimitar0811@users.noreply.github.com>
2026-04-17 17:38:21 +0300

    Added Nunit project

---
+++ IdeaCenterDZHA/Models/ApiResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace IdeaCenterDZHA.Models
+{
+    internal class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+
+        public string? Msg { get; set; }
+
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+    }
+}
---
+++ IdeaCenterDZHA/Models/IdeaDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace IdeaCenterDZHA.Models
+{
+    internal class IdeaDTO
+    {
+        [JsonPropertyName("title")]
+
+        public string? Title { get; set; }
+
+        [JsonPropertyName("description")]
+
+        public string? Description { get; set; }
+
+        [JsonPropertyName("url")]
+
+        public string? Url { get; set; }
+
+    }
+}
